b428ab17b4d8f19635723b10f212216b1c29fc44
oreste <muhirwa.g.oreste@gmail.com>
2026-01-31 17:43:34 +0100

    Update UserModel and implement privacy-aware name display logic
    - Rename `name` field to `fullName` in `UserModel`.
    - Add `PrivacyLevel` enum and integrate it into the `UserModel`.
    - Implement `getDisplayName` utility function to conditionally obfuscate user names based on privacy settings and contact status.

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,15 +1,44 @@
 import 'package:kgl_express/core/enums/user_role.dart';
+import 'package:kgl_express/core/utils/string_utils.dart';
 
 class UserModel {
   final String uid;
-  final String name;
+  final String fullName;
   final String phoneNumber;
   final UserRole role;
+  final PrivacyLevel privacy;
 
   UserModel({
     required this.uid,
-    required this.name,
+    required this.fullName,
     required this.phoneNumber,
     required this.role,
+    this.privacy = PrivacyLevel.contactsOnly,
   });
+}
+
+
+enum PrivacyLevel {
+  public,
+  contactsOnly,
+  private
+}
+
+String getDisplayName({
+  required UserModel recipient,
+  required bool isSavedInMyContacts,
+}) {
+  switch (recipient.privacy) {
+    case PrivacyLevel.public:
+      return recipient.fullName;
+
+    case PrivacyLevel.private:
+      return obfuscateName(recipient.fullName);
+
+    case PrivacyLevel.contactsOnly:
+    // This is the "Smart" logic
+      return isSavedInMyContacts
+          ? recipient.fullName
+          : obfuscateName(recipient.fullName);
+  }
 }
