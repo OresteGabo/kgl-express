6ff7b92b6219811be1b3306af6405a5597894900
oreste <muhirwa.g.oreste@gmail.com>
2026-02-02 13:36:15 +0100

    Update app theme implementation in main.dart
    - Import `core/theme/theme.dart` and initialize `MaterialTheme`.
    - Refactor `KGLExpressApp` to use generated light and dark themes with system mode support.
    - Remove `testDatabase` call and comment out legacy app/database test code.
    - Clean up unused imports and whitespace.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:kgl_express/features/sender/presentation/home_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:kgl_express/core/theme/app_theme.dart';
 
+import 'core/theme/theme.dart';
+
 
 void main() async {
   // Ensure Flutter is initialized before calling SharedPreferences
@@ -11,11 +13,10 @@ void main() async {
 
   final prefs = await SharedPreferences.getInstance();
   final String? savedRole = prefs.getString('default_role');
-  testDatabase();
 
   runApp(KGLExpressApp(initialRole: savedRole));
 }
-
+/*
 class KGLExpressApp extends StatelessWidget {
   final String? initialRole;
   const KGLExpressApp({super.key, this.initialRole});
@@ -35,13 +36,32 @@ class KGLExpressApp extends StatelessWidget {
 }
 
 Future<void> testDatabase() async {
-
-
-
-
-
 }
+*/
 
+
+class KGLExpressApp extends StatelessWidget {
+  final String? initialRole;
+  const KGLExpressApp({super.key, this.initialRole});
+
+  @override
+  Widget build(BuildContext context) {
+    // Initialize the theme with your app's TextTheme
+    final materialTheme = MaterialTheme(Theme.of(context).textTheme);
+
+    return MaterialApp(
+      title: 'KGL Express',
+      // Using your generated light theme
+      theme: materialTheme.light(),
+      // Using your generated dark theme
+      darkTheme: materialTheme.dark(),
+      themeMode: ThemeMode.system,
+      home: initialRole == null
+          ? const OnboardingRoleScreen()
+          : const PlaceholderHomeScreen(),
+    );
+  }
+}
 
 
 
