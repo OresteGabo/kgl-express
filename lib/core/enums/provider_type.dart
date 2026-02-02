46d8d9ebf060eb144d0102d398f01e10783a4d9e
oreste <muhirwa.g.oreste@gmail.com>
2026-02-02 18:36:17 +0100

    Add Speciality enum to provider_type.dart
    - Define `Speciality` enum with labels and icons for various service categories including Mechanic, Electrician, Plumber, and others.

--- lib/core/enums/provider_type.dart
+++ lib/core/enums/provider_type.dart
@@ -22,3 +22,63 @@ enum ProviderType {
     required this.description,
   });
 }
+
+
+enum Speciality{
+  mechanic(
+    label:"Mechanic",
+    icon:Icons.settings,
+  ),
+  electrician(
+    label:"Electrician",
+    icon:Icons.bolt,
+  ),
+  plumber(
+    label:"Plumber",
+    icon:Icons.plumbing,
+  ),
+  houseCleaning(
+    label:"House Cleaning",
+    icon:Icons.cleaning_services,
+  ),
+  carpenter(
+    label:"Carpenter",
+    icon:Icons.handyman,
+  ),
+  painter(
+    label:"Painter",
+    icon:Icons.format_paint,
+  ),
+  masonry(
+    label:"Masonry & construction",
+    icon:Icons.foundation,
+  ),
+  welder(
+    label:"Professional Welder",
+    icon:Icons.precision_manufacturing,
+  ),
+  tiler(
+    label:"Tiling & Flooring",
+    icon:Icons.grid_view,
+  ),
+  acRepair(
+    label:"AC Repair",
+    icon:Icons.ac_unit,
+  ),
+  laundry(
+    label:"Laundry",
+    icon:Icons.local_laundry_service,
+  ),
+  landscaper(
+    label:"Landscaping",
+    icon:Icons.landscape,
+  );
+
+  final String label;
+  final IconData icon;
+
+  const Speciality({
+    required this.label,
+    required this.icon,
+  });
+}
